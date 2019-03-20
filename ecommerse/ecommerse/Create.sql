1ddd25a36cefea40a18dbbc490d83ad37199f63c
hannaell <hannaell91@gmail.com>
2019-03-20 06:40:28 +0100

    Update Create.sql

--- ecommerse/ecommerse/Create.sql
+++ ecommerse/ecommerse/Create.sql
@@ -1,24 +1,36 @@
-﻿CREATE TABLE Order_Products (
-    products_id int NOT NULL,
-    orders_id int NOT NULL
-    PRIMARY KEY (orders_id, products_id)  
-);
-
-CREATE TABLE Orders (
-    id int NOT NULL,
-    name varchar(128) NOT NULL,
+﻿CREATE TABLE Orders (
+    Id int NOT NULL AUTO_INCREMENT,
+    Firstname varchar(128) NOT NULL,
+    Lastname varchar(128) NOT NULL,
+    Adress varchar(128) NOT NULL,
+    Zipcode int NOT NULL,
+    City text NOT NULL,
+    Paid bool,
     CONSTRAINT Orders_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE Products (
-    id int NOT NULL,
-    name varchar(128) NOT NULL,
-    description text NOT NULL,
+    Id int NOT NULL AUTO_INCREMENT,
+    Product varchar(128) NOT NULL,
+    Description text NOT NULL,
+    Price int NOT NULL,
+    Quantity int NOT NULL,
     CONSTRAINT Products_pk PRIMARY KEY (id)
 );
 
-ALTER TABLE Order_Products ADD CONSTRAINT Order_Products_Orders FOREIGN KEY Order_Products_Orders (orders_id) 
-    REFERENCES Orders (id);
+CREATE TABLE Order_Products (
+    products_id int NOT NULL,
+    orders_id int NOT NULL,
+    PRIMARY KEY (orders_id, products_id),
+    
+    FOREIGN KEY (products_id) REFERENCES Products(id),
+    FOREIGN KEY (orders_id) REFERENCES Orders(id)
+);
 
-ALTER TABLE Order_Products ADD CONSTRAINT Order_Products_Products FOREIGN KEY Order_Products_Products (products_id)
-    REFERENCES Products (id);
+
+
+
+
+SELECT Orders.* , Products.* FROM Order_Products
+LEFT JOIN Orders ON Order_Products.orders_id = orders_id
+LEFT JOIN Products ON Order_Products.products_id = products_id
